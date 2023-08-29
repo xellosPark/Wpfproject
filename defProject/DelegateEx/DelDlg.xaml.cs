0f39298de42e0dd33d1a138d75479cf321be8dce
ParkDongJun <yuki@ubisam.com>
2023-08-29 16:35:35 +0900

    delegate 예제

--- defProject/DelegateEx/DelDlg.xaml.cs
+++ defProject/DelegateEx/DelDlg.xaml.cs
@@ -19,9 +19,184 @@ namespace defProject.DelegateEx
     /// </summary>
     public partial class DelDlg : Window
     {
+        public delegate int delFuncDow_Edge(int i);
+        public delegate int delFuncTopping(string strOrder, int Ea);
+
+ 
+
+        int _iTotalPrice = 0;
         public DelDlg()
         {
             InitializeComponent();
         }
+
+        private void btnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            delFuncDow_Edge delDow = new delFuncDow_Edge(fDow);
+            delFuncDow_Edge delEdge = new delFuncDow_Edge(fEdge);
+
+            delFuncTopping delTopping = null;
+
+            int iDowOrder = 0;
+            int iEdgeOrder = 0;
+
+            // 선택 도우 확인
+            if (rdoDow1.IsChecked == true)
+            {
+                iDowOrder = 1;
+                //delDow(1);
+                //fDow(1);
+            }
+            else if (rdoDow2.IsChecked == true)
+            {
+                iDowOrder = 2;
+                //delDow(2);
+                //fDow(2);
+            }
+
+            //delDow(iDowOrder);
+
+            // 선택 엣지 확인
+            if (rdoEdge1.IsChecked == true)
+            {
+                iEdgeOrder = 1;
+            }
+            else if (rdoEdge2.IsChecked == true)
+            {
+                iEdgeOrder = 2;
+            }
+
+            //delEdge(iEdgeOrder);
+
+            fCallBackDelegate(iDowOrder, delDow);
+            fCallBackDelegate(iEdgeOrder, delEdge);
+
+            // 토핑 선택 확인
+            if (cboxTopping1.IsChecked == true)
+            {
+                //delTopping = new delFuncTopping(fTopping1);
+                delTopping += fTopping1;
+            }
+
+            if (cboxTopping2.IsChecked == true) delTopping += fTopping2;
+            if (cboxTopping3.IsChecked == true) delTopping += fTopping3;
+
+            string currentSpinValue = (string)numCout.Content;
+            int initialValue = String.IsNullOrEmpty(currentSpinValue) ? 0 : Convert.ToInt32(currentSpinValue);
+
+            delTopping("토핑", (int)initialValue);
+
+            flboxOrderRed("----------------------------------");
+            flboxOrderRed(string.Format("전체 주문 가격은 {0}원 입니다.", _iTotalPrice));
+        }
+
+        #region Function
+        /// <summary>
+        /// 0: 선택 안함 1: 오리지널 2 : 씬
+        /// </summary>
+        /// <param name="iOrder"></param>
+        /// <returns></returns>
+        private int fDow(int iOrder)
+        {
+            string strOrder = string.Empty;
+            int iPrice = 0;
+
+            if (iOrder == 1)
+            {
+                iPrice = 10000;
+                strOrder = string.Format("도우는 오리지널을 선택 하셨습니다. ({0}원)", iPrice.ToString());
+            }
+            else if (iOrder == 2)
+            {
+                iPrice = 11000;
+                strOrder = string.Format("도우는 씬을 선택 하셨습니다. ({0}원)", iPrice.ToString());
+            }
+            else
+            {
+                strOrder = "도우를 선택하지 않았습니다.";
+            }
+
+            flboxOrderRed(strOrder);
+
+            return _iTotalPrice = _iTotalPrice + iPrice;
+        }
+
+        /// <summary>
+        /// O : 선택안함, 1 : 리치골드, 2 : 치즈크러스터
+        /// </summary>
+        /// <param name="iOrder"></param>
+        /// <returns></returns>
+        private int fEdge(int iOrder)
+        {
+            string strOrder = string.Empty;
+            int iPrice = 0;
+
+            if (iOrder == 1)
+            {
+                iPrice = 1000;
+                strOrder = string.Format("엣지는 리치골드를 선택 하셨습니다. ({0}원)", iPrice.ToString());
+            }
+            else if (iOrder == 2)
+            {
+                iPrice = 2000;
+                strOrder = string.Format("엣지는 치즈크러스터를 선택 하셨습니다. ({0}원)", iPrice.ToString());
+            }
+            else
+            {
+                strOrder = "엣지는 선택하지 않았습니다.";
+            }
+
+            flboxOrderRed(strOrder);
+
+            return _iTotalPrice = _iTotalPrice + iPrice;
+        }
+
+        public int fCallBackDelegate(int i, delFuncDow_Edge dFunction)
+        {
+            return dFunction(i);
+        }
+
+        private int fTopping1(string Order, int iEa)
+        {
+            string strOrder = string.Empty;
+            int iPrice = iEa * 500;
+
+            strOrder = string.Format("소세지 {0} {1} 개를 선택 하였습니다. : ({2}원 (1ea 500원)", Order, iEa, iPrice);
+
+            flboxOrderRed(strOrder);
+
+            return _iTotalPrice = _iTotalPrice + iPrice;
+        }
+
+        private int fTopping2(string Order, int iEa)
+        {
+            string strOrder = string.Empty;
+            int iPrice = iEa * 200;
+
+            strOrder = string.Format("감자 {0} {1} 개를 선택 하였습니다. : ({2}원 (1ea 200원)", Order, iEa, iPrice);
+
+            flboxOrderRed(strOrder);
+
+            return _iTotalPrice = _iTotalPrice + iPrice;
+        }
+
+        private int fTopping3(string Order, int iEa)
+        {
+            string strOrder = string.Empty;
+            int iPrice = iEa * 300;
+
+            strOrder = string.Format("치즈 {0} {1} 개를 선택 하였습니다. : ({2}원 (1ea 300원)", Order, iEa, iPrice);
+
+            flboxOrderRed(strOrder);
+
+            return _iTotalPrice = _iTotalPrice + iPrice;
+        }
+
+
+        private void flboxOrderRed(string strOrder)
+        {
+            lboxOrder.Items.Add(strOrder);
+        }
+        #endregion
     }
 }
