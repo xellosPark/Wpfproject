444fa6422012a35d1508f5bbdf99f8b74c004d7a
ParkDongJun <yuki@ubisam.com>
2023-08-30 16:52:02 +0900

    delegateevent dlg 완료

--- defProject/DelegateEx/DelDlg.xaml.cs
+++ defProject/DelegateEx/DelDlg.xaml.cs
@@ -25,9 +25,13 @@ namespace defProject.DelegateEx
  
 
         int _iTotalPrice = 0;
+        public int initialValue { get; set; } = 0;
+
+        SubDelDlg Sundlg { get; set; } = null;
         public DelDlg()
         {
             InitializeComponent();
+         
         }
 
         private void btnOrder_Click(object sender, RoutedEventArgs e)
@@ -37,6 +41,8 @@ namespace defProject.DelegateEx
 
             delFuncTopping delTopping = null;
 
+            Dictionary<string, int> dPizzaOrder = new Dictionary<string, int>(); // 주문을 담을 오더 (Key : 주문 종류, value : 개수)
+
             int iDowOrder = 0;
             int iEdgeOrder = 0;
 
@@ -44,26 +50,33 @@ namespace defProject.DelegateEx
             if (rdoDow1.IsChecked == true)
             {
                 iDowOrder = 1;
+                dPizzaOrder.Add("오리지널", 1);
                 //delDow(1);
                 //fDow(1);
             }
             else if (rdoDow2.IsChecked == true)
             {
                 iDowOrder = 2;
+                dPizzaOrder.Add("씬", 1);
                 //delDow(2);
                 //fDow(2);
             }
 
             //delDow(iDowOrder);
 
+            string currentSpinValue = (string)numCout.Content;
+            initialValue = String.IsNullOrEmpty(currentSpinValue) ? 0 : Convert.ToInt32(currentSpinValue);
+
             // 선택 엣지 확인
             if (rdoEdge1.IsChecked == true)
             {
                 iEdgeOrder = 1;
+                dPizzaOrder.Add("리치골드", 1);
             }
             else if (rdoEdge2.IsChecked == true)
             {
                 iEdgeOrder = 2;
+                dPizzaOrder.Add("치즈크러스터", 1);
             }
 
             //delEdge(iEdgeOrder);
@@ -76,26 +89,36 @@ namespace defProject.DelegateEx
             {
                 //delTopping = new delFuncTopping(fTopping1);
                 delTopping += fTopping1;
+                dPizzaOrder.Add("소세지", (int)initialValue);
             }
 
-            if (cboxTopping2.IsChecked == true) delTopping += fTopping2;
-            if (cboxTopping3.IsChecked == true) delTopping += fTopping3;
+            if (cboxTopping2.IsChecked == true)
+            {
+                delTopping += fTopping2;
+                dPizzaOrder.Add("감자", (int)initialValue);
+            }
+                
+            if (cboxTopping3.IsChecked == true)
+            {
+                delTopping += fTopping3;
+                dPizzaOrder.Add("치즈", (int)initialValue);
+            }
+                
 
             if(cboxTopping1.IsChecked == false && cboxTopping2.IsChecked == false && cboxTopping3.IsChecked == false)
             {
                 flboxOrderRed("토핑을 선택해주세요!");
                 return;
             }
-
-
-            string currentSpinValue = (string)numCout.Content;
-            int initialValue = String.IsNullOrEmpty(currentSpinValue) ? 0 : Convert.ToInt32(currentSpinValue);
-
             delTopping("토핑", (int)initialValue);
 
             flboxOrderRed("----------------------------------");
             flboxOrderRed(string.Format("전체 주문 가격은 {0}원 입니다.", _iTotalPrice));
+
+            SubLoadling(dPizzaOrder);
         }
+
+
 
         #region Function
         /// <summary>
@@ -204,6 +227,58 @@ namespace defProject.DelegateEx
         {
             lboxOrder.Items.Add(strOrder);
         }
+
+        private void numCout_Spin(object sender, Xceed.Wpf.Toolkit.SpinEventArgs e)
+        {
+            Xceed.Wpf.Toolkit.ButtonSpinner spinner = (Xceed.Wpf.Toolkit.ButtonSpinner)sender;
+            string currentSpinValue = (string)spinner.Content;
+
+            initialValue = String.IsNullOrEmpty(currentSpinValue) ? 0 : Convert.ToInt32(currentSpinValue);
+
+            if (e.Direction == Xceed.Wpf.Toolkit.SpinDirection.Increase)
+            { initialValue++; }
+            else
+            { initialValue--; }
+
+            spinner.Content = initialValue.ToString();
+        }
+
         #endregion
+
+        #region event_예제
+        private void SubLoadling(Dictionary<string, int> dPizzaOrder)
+        {
+            if (Sundlg != null)
+            {
+                Sundlg.Close();
+                Sundlg = null;
+            }
+
+            Sundlg = new SubDelDlg();
+            Sundlg.eventdelPizzaComplete += Sundlg_eventdelPizzaComplete;
+            Sundlg.Show();
+
+            Sundlg.fPizzrCheck(dPizzaOrder);
+        }
+
+        private int Sundlg_eventdelPizzaComplete(string strResult, int iTime)
+        {
+            flboxOrderRed("----------------------------------");
+            flboxOrderRed(string.Format("{0} / 걸린 시간 : {1}", strResult, iTime));
+
+            // 결과 값을 자식 form으로 return 해주기 위해 사용
+            // 시간 계산을 해서 5분이 넘어 가면 -1
+            if (iTime > 4000)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        #endregion
+
     }
 }
