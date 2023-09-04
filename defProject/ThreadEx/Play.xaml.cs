16711606c6fcc0f61114482b960b885572438369
ParkDongJun <yuki@ubisam.com>
2023-09-04 17:10:09 +0900

    Theared 작업중

---
+++ defProject/ThreadEx/Play.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace defProject.ThreadEx
+{
+    /// <summary>
+    /// Play.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Play : Window
+    {
+        public delegate int delMessage(object sender, string strResult);  // delgate 선언
+        public event delMessage eventdelMessage;
+
+        private string strPlayerName = string.Empty;
+
+        public string StrPlayerName { get => strPlayerName; set => strPlayerName = value; }
+
+        Thread _thread = null;
+
+        public Play()
+        {
+            InitializeComponent();
+        }
+        public Play(string strPlayerName)   
+        {
+            InitializeComponent();
+
+            lblPlayerName.Content = StrPlayerName = strPlayerName;
+
+        }
+
+        public void fThreadStart()
+        {
+            //_thread = new Thread(new ThreadStart(Run));   // ThreadStart 델리게이트 타입 객체를 생성 후 함수를 넘김
+
+            _thread = new Thread(Run);   // 컴파일러에서 델리게이트 객체를 추론해서 생성 후 생성 후 함수를 넘김 (new ThreadStart 생략)
+
+            //_thread = new Thread(delegate () { Run(); });   // 익명메소드를 사용하여 생성 후 함수를 넘김
+
+            _thread.Start();
+        }
+
+        private void Run()
+        {
+            // UI Control이 자신이 만들어진 Thread가 아닌 다른 Thread에서 접근할 경우 Cross Thread가 발생
+            //CheckForIllegalCrossThreadCalls = false;   // Thread 충돌에 대한 예외 처리를 무시 (Cross Thread 무시)
+
+            int ivar = 0;
+            Random rd = new Random();
+
+            while (proBar.Value < 100)
+            {
+                // UI 스레드 체크: 호출한 스레드가 작업 스레드인지 확인
+                if (Dispatcher.CheckAccess())
+                {
+                    // UI 스레드인 경우
+                    proBar.Value = ivar; // 이 부분은 e 변수에 대한 정보가 없어서 수정이 필요할 수 있습니다.
+                    lblProcess.Content = $"{ivar} %";
+                }
+                else
+                {
+                    // UI 스레드가 아닌 경우 Dispatcher.Invoke를 사용하여 UI 업데이트
+                    Dispatcher.Invoke(() =>
+                    {
+                        proBar.Value = ivar; // 이 부분은 e 변수에 대한 정보가 없어서 수정이 필요할 수 있습니다.
+                        lblProcess.Content = $"{ivar} %";
+                    });
+                }
+
+                // ivar 값 업데이트
+                ivar = rd.Next(1, 11);
+
+                // 작업 스레드를 잠시 대기
+                Thread.Sleep(300);
+            }
+
+
+            eventdelMessage(this, "완주!! (Thread Complete)");
+        }
+
+    }
+}
---
+++ defProject/ThreadEx/ThreadDlg.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace defProject.ThreadEx
+{
+    /// <summary>
+    /// ThreadDlg.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ThreadDlg : Window
+    {
+        public int initialValue { get; set; } = 0;
+
+        private enum enumPlayer
+        {
+            아이린,
+            슬기,
+            웬디,
+            조이,
+            예리,
+        }
+
+        int _locationX = 0;
+        int _locationY = 0;
+
+        public ThreadDlg()
+        {
+            InitializeComponent();
+
+            _locationX = (int)this.Left;
+            _locationY = (int)this.Top;
+        }
+
+        private void numCout_Spin(object sender, Xceed.Wpf.Toolkit.SpinEventArgs e)
+        {
+            Xceed.Wpf.Toolkit.ButtonSpinner spinner = (Xceed.Wpf.Toolkit.ButtonSpinner)sender;
+            string currentSpinValue = (string)spinner.Content;
+
+            initialValue = String.IsNullOrEmpty(currentSpinValue) ? 0 : Convert.ToInt32(currentSpinValue);
+
+            if (e.Direction == Xceed.Wpf.Toolkit.SpinDirection.Increase)
+            { initialValue++; }
+            else
+            { initialValue--; }
+
+            spinner.Content = initialValue.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _locationX = (int)this.Left + (int)this.Width;
+            _locationY = (int)this.Top;
+
+            string currentSpinValue = (string)numCout.Content;
+            initialValue = String.IsNullOrEmpty(currentSpinValue) ? 0 : Convert.ToInt32(currentSpinValue);
+
+            for (int i = 0; i < initialValue; i++)
+            {
+                Play pl = new Play(((enumPlayer)i).ToString());
+
+                // 위치 설정
+                pl.Left = _locationX;
+                pl.Top = _locationY + pl.Height * i;
+
+                // 이벤트 핸들러 등록
+                pl.eventdelMessage += Pl_eventdelMessage;
+
+                // 창을 표시
+                pl.Show();
+
+                // 작업 시작
+                pl.fThreadStart();
+            }
+        }
+
+        private int Pl_eventdelMessage(object sender, string strResult)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                // 현재 스레드가 UI 스레드인 경우
+                Play oPlayerForm = sender as Play;
+                lboxResult.Items.Add(string.Format("Player : {0}, Text : {1}", oPlayerForm.StrPlayerName, strResult));
+            }
+            else
+            {
+                // 다른 스레드에서 UI 업데이트를 수행해야 하는 경우
+                Dispatcher.Invoke(() =>
+                {
+                    Play oPlayerForm = sender as Play;
+                    lboxResult.Items.Add(string.Format("Player : {0}, Text : {1}", oPlayerForm.StrPlayerName, strResult));
+                });
+            }
+            return 0;
+        }
+    }
+}
